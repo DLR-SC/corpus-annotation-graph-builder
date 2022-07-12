f6900b4ffb3f38d596d172be6874d2f5ad3ff157
El Baff, Roxanne <roxanne.elbaff@dlr.de>
2022-07-12 11:46:23 +0200

    added cleanup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='graph_framework',
-    version='0.1.0',
+    version='0.2.0',
     description='This is a general framework to create arango db graphs',
     # url='',
     author='DLR',
@@ -12,5 +12,7 @@ setup(
               'graph_framework.components',
               'graph_framework.graph',
               'graph_framework.utils'],
-    install_requires=['pyArango'],
+    install_requires=['pyArango',
+                      "arango",
+                      ],
 )
