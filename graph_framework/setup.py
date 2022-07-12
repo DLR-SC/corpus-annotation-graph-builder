9342b03eded6e5b5293392ef5d69935bc344e283
Benedikt Kantz <benedikt.kantz@dlr.de>
2022-07-12 11:40:33 +0200

    Remove nonexistent packages

--- graph_framework/setup.py
+++ graph_framework/setup.py
@@ -13,8 +13,6 @@ setup(
               'graph_framework.graph',
               'graph_framework.utils'],
     install_requires=['pyArango',
-                      'python-arango',
-                      "arango",
-                      "dataclasses",
+                      'python-arango'
                       ],
 )
