a8f2f4c279cd5a8ddd8bfabd0ce4233b59d266c9
Benedikt Kantz <benedikt.kantz@dlr.de>
2022-08-01 16:38:55 +0200

    Fix broken install

--- setup.py
+++ setup.py
@@ -14,17 +14,18 @@ setup(
                       'python-arango',
                       'nltk',
                       'networkx',
-                      'pyvis'
+                      'pyvis',
                       'tomli >= 1.1.0',
                       'spacy',
-                      'markupsafe<=2.0.1' # for spacy - it uses soft_unicode which was removed from version > 2.0.1
+                      # for spacy - it uses soft_unicode which was removed from version > 2.0.1
+                      'markupsafe<=2.0.1'
                       ],
     classifiers=[
-          'Development Status :: 2 - Pre-Alpha',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python',
-      ],
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
     zip_safe=False,
     include_package_data=True,
     package_data={"configs": ["*.toml"]},
