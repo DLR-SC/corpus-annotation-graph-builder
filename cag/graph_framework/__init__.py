97be004582c3caf04b32e3f522b4a9acc1f73b24
El Baff, Roxanne <roxanne.elbaff@dlr.de>
2022-10-26 12:56:12 +0200

    fixed blocker issue #8

--- cag/graph_framework/__init__.py
+++
@@ -1,3 +0,0 @@
-
-""" Generic graph package """
-__version__ = "0.7.0"
--- cag/graph_framework/graph/base_graph.py
+++ cag/graph_framework/graph/base_graph.py
@@ -4,6 +4,8 @@ import json
 
 from pyArango.theExceptions import CreationError
 
+from cag import logger
+
 
 class BaseGraph(Graph):
     _edgeDefinitions = [EdgeDefinition('GenericEdge', fromCollections=['GenericNode'], toCollections=['GenericNode'])]
@@ -15,7 +17,7 @@ class BaseGraph(Graph):
             if not self.database.hasCollection(col):
                 self.database.createCollection(col)
 
-    def update_graph_structure(self, relation, from_collections, to_collections, create_collections=True, waitForSync = True):
+    def update_graph_structure(self, relation, from_collections, to_collections, create_collections=True, waitForSync = False):
         """
         Adds an edge definition to the graph if it not already exists. Otherwhise the existing edge definition becomes updated if necessary based on the given collections.
         In case of an update, this method never deletes collections from an edge definition but creates a union of the existing and provided collections.
@@ -29,47 +31,55 @@ class BaseGraph(Graph):
         Returns:
             Boolean value indicating if the edge definitions of this graph have been updated.
         """
+        is_new_relation = True
         if relation in self.definitions.keys():
-            print("in relation")
+            is_new_relation = False
+
             existings_froms = set(self.definitions[relation].fromCollections)
             existing_tos = set(self.definitions[relation].toCollections)
+
             proposed_froms = set(from_collections)
             proposed_tos = set(to_collections)
+
             new_froms = proposed_froms - existings_froms
             new_tos = proposed_tos - existing_tos
 
-            if len(new_froms) > 0:
-                print(f"new {new_froms}")
-                url = '%s/edge/%s' % (self.getURL(), relation)
+            if create_collections and len(new_froms) > 0:
+                self._check_and_update_collections(new_froms)
 
-                if create_collections:
-                    self._check_and_update_collections(new_froms)
 
-            if len(new_tos) > 0:
-                print(f"new {new_tos}")
-                url = '%s/edge/%s' % (self.getURL(), relation)
-
-                if create_collections:
-                    self._check_and_update_collections(new_tos)
+            if create_collections and (len(new_tos) > 0):
+                self._check_and_update_collections(new_tos)
 
             if (len(new_froms ) > 0) or (len(new_tos) > 0):
                  url = '%s/edge/%s' % (self.getURL(), relation)
                  from_collections = list(existings_froms | new_froms)
                  to_collections = list(existing_tos | new_tos)
             else:
+                logger.debug(f"edge  found {relation} but has no changes")
                 return False
+
+            logger.debug(f"edge  found {relation}, updating it")
+            r = self.connection.session.put(url, data=json.dumps(
+                {
+                    'collection': relation,
+                    'from': from_collections,
+                    'to': to_collections
+                }, default=str), params={'waitForSync': waitForSync}
+                                            )
         else:
-            print(f"edge not found {self.getURL()}")
+            logger.debug(f"edge not found {relation}, adding it")
             url = '%s/edge' % self.getURL()
-        #url = '%s/edge' % self.getURL()
-        r = self.connection.session.post(url, data = json.dumps(
-            {
-                'collection': relation,
-                '_from': from_collections,
-                '_to': to_collections
-            }, 
-            default=str), params = {'waitForSync' : waitForSync}
-        )
+            r = self.connection.session.post(url, data=json.dumps(
+                {
+                    'collection': relation,
+                    'from': from_collections,
+                    'to': to_collections
+                },
+                default=str), params={'waitForSync': waitForSync}
+                                             )
+
+
 
         data = r.json()
         if r.status_code == 201 or r.status_code == 202:
