34ade8cf21319427f6b2c900745e6ea9266b1cbd
El Baff, Roxanne <roxanne.elbaff@dlr.de>
2022-12-07 15:06:58 +0100

    import error

--- cag/framework/__init__.py
+++ cag/framework/__init__.py
@@ -1,4 +1,4 @@
 from .annotator.annotator_base import GenericAnnotator
-from .creator.graph_creator_base import GraphCreatorBase
+from .creator.base_creator import GraphCreatorBase
 from .analyzer.analyzer_base import AnalyzerBase
 __all__ = [GenericAnnotator, GraphCreatorBase, AnalyzerBase]
