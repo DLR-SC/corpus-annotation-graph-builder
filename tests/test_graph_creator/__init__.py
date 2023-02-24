4f74581c6e81244da7543e4e17c3a41d1750269f
Norman Nabhan <norman.nabhan@dlr.de>
2023-02-24 16:06:26 +0100

    + Update ArangoDB Service definition for tests

--- tests/test_graph_creator/__init__.py
+++ tests/test_graph_creator/__init__.py
@@ -3,7 +3,7 @@ import os
 from cag.utils.config import Config
 
 config = Config(
-    url=f"http://localhost:8529",
+    url=f"http://{os.environ['ARANGO_HOST']}:{os.environ['ARANGO_PORT']}",
     user="root",
     password="",
     graph="SampleGraph",
