cf473980e183ba4e1e302316d8e305a41ddbf43e
Norman Nabhan <norman.nabhan@dlr.de>
2023-03-01 14:07:57 +0100

    + Update ArangoDB Service definition for tests

--- tests/test_graph_creator/test_simple_init.py
+++ tests/test_graph_creator/test_simple_init.py
@@ -1,9 +1,12 @@
+import os
+
 import pytest
 
 from cag.framework import GraphCreatorBase
 from cag.graph_elements.nodes import GenericOOSNode, Field
 from cag.graph_elements.relations import GenericEdge
-from . import config
+from cag.utils.config import Config
+from tests.test_graph_creator import config_factory
 
 
 class CollectionA(GenericOOSNode):
@@ -25,21 +28,25 @@ class HasRelation(GenericEdge):
     _fields = GenericEdge._fields
 
 
+class HasAnotherRelation(GenericEdge):
+    _fields = GenericEdge._fields
+
+
 class SampleGraphCreator(GraphCreatorBase):
     _name = "SampleGraphCreator"
     _description = "Graph based on the DLR elib corpus"
 
     _edge_definitions = [
         {
-            'relation': "HasRelation",
-            'from_collections': [CollectionA._name],
-            'to_collections': [CollectionB._name]
+            "relation": "HasRelation",
+            "from_collections": [CollectionA._name],
+            "to_collections": [CollectionB._name],
         },
         {
-            'relation': "HasAnotherRelation",
-            'from_collections': [CollectionC._name],
-            'to_collections': [CollectionC._name]
-        }
+            "relation": "HasAnotherRelation",
+            "from_collections": [CollectionC._name],
+            "to_collections": [CollectionC._name],
+        },
     ]
 
     def init_graph(self):
@@ -48,39 +55,69 @@ class SampleGraphCreator(GraphCreatorBase):
 
 class TestGCBasics:
     def test_arango_connection(self):
-        assert config.arango_db.name == "test"
+        config = config_factory()
+        assert config.arango_db.name == config.database
 
     def test_create_collection(self):
-        SampleGraphCreator("", config, initialize=False)
+        config = config_factory()
+        SampleGraphCreator("", config_factory())
         assert config.arango_db.has_collection("CollectionA")
         assert config.arango_db.has_collection("CollectionB")
         assert config.arango_db.has_collection("HasRelation")
 
     def test_node_creation(self):
-        gc = SampleGraphCreator("", config, initialize=False)
+        config = config_factory()
+        gc = SampleGraphCreator("", config)
 
-        node1 = gc.upsert_node(CollectionA._name, {"value": "val1", "_key": "v1"})
-        assert config.arango_db.collection("CollectionA").get("v1") == node1.getStore()
+        node1 = gc.upsert_node(
+            CollectionA._name, {"value": "val1", "_key": "v1"}
+        )
+        assert (
+            config.arango_db.collection("CollectionA").get("v1")
+            == node1.getStore()
+        )
 
-        node2 = gc.upsert_node(CollectionB._name, {"value": "val2", "_key": "v2"})
-        assert config.arango_db.collection("CollectionB").get("v2") == node2.getStore()
+        node2 = gc.upsert_node(
+            CollectionB._name, {"value": "val2", "_key": "v2"}
+        )
+        assert (
+            config.arango_db.collection("CollectionB").get("v2")
+            == node2.getStore()
+        )
 
         edge = gc.upsert_edge("HasRelation", node1, node2)
-        assert config.arango_db.collection("HasRelation").get("CollectionA-v1-CollectionB-v2") == edge.getStore()
+        assert (
+            config.arango_db.collection("HasRelation").get(
+                "CollectionA-v1-CollectionB-v2"
+            )
+            == edge.getStore()
+        )
 
     def test_node_upsert(self):
-        gc = SampleGraphCreator("", config, initialize=False)
-        node = gc.upsert_node(CollectionA._name, {"value": "val1", "value2": "no change", "_key": "v1"})
-        node2 = gc.upsert_node(CollectionA._name, {"value": "val2", "_key": "v1"})
+        config = config_factory()
+        gc = SampleGraphCreator("", config)
+        node = gc.upsert_node(
+            CollectionA._name,
+            {"value": "val1", "value2": "no change", "_key": "v1"},
+        )
+        node2 = gc.upsert_node(
+            CollectionA._name, {"value": "val2", "_key": "v1"}
+        )
         assert node["_rev"] != node2["_rev"]
         assert node["timestamp"] != node2["timestamp"]
         assert node2["value"] == "val2"
         assert node2["value2"] == "no change"
 
     def test_edge_cases(self):
-        gc = SampleGraphCreator("", config, initialize=False)
+        config = config_factory()
+        gc = SampleGraphCreator("", config)
         with pytest.raises(KeyError):
-            gc.upsert_node("UnknownCollection", {"value": "val1", "value2": "no change", "_key": "v1"})
-        node = gc.upsert_node(CollectionC._name, {"value": "val1", "value2": "no change", "_key": "v1"})
-        # this shouldn't work
+            gc.upsert_node(
+                "UnknownCollection",
+                {"value": "val1", "value2": "no change", "_key": "v1"},
+            )
+        node = gc.upsert_node(
+            CollectionC._name,
+            {"value": "val1", "value2": "no change", "_key": "v1"},
+        )
         edge = gc.upsert_edge("HasRelation", node, node)
